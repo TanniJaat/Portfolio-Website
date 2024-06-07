c60973711d822a5a0be5d4b2108082b917909694
TanniJaat <tprimegamers@gmail.com>
2024-06-07 16:59:04 +0530

    Update ExButton.tsx

--- src/components/ExButton.tsx
+++ src/components/ExButton.tsx
@@ -10,7 +10,7 @@ function ExButton({text,link,className , target}:{
 }) {
   return (
    <div>
-    <Link target={target} href={link} className={cn(" shadow-[inset_0_0_0_2px_#616467] text-black lg:px-6 md:px-6 lg:py-4 md:py-4 px-4 py-2 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-black hover:text-white dark:text-neutral-200 transition duration-200",className)}>
+    <Link target={target} href={link} className={cn(" shadow-[inset_0_0_0_2px_#616467] text-black lg:px-6 md:px-6 lg:py-4 md:py-4 px-4 py-2 rounded-full tracking-tighter uppercase font-bold bg-transparent hover:bg-black hover:text-white dark:text-neutral-200 transition duration-200",className)}>
         {text}
      </Link>
    </div>
